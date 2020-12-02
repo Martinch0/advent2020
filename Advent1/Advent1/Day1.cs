a43fd8f59d0b16c0afbef7d191971224f466d488
Martin Mihov <martinsmihov@gmail.com>
2020-12-02 20:48:16 +0000

    Refactor for Day1

---
+++ Advent1/Advent1/Day1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Advent1
+{
+	class Day1
+	{
+		private string inputPath = "../../../../Input/Input1a.txt";
+
+		private void ReadInputs(string file, ref List<int> numbers)
+		{
+			if (numbers == null)
+			{
+				numbers = new List<int>();
+			}
+
+			numbers.Clear();
+
+			string[] lines = File.ReadAllLines(file);
+
+			foreach (string n in lines)
+			{
+				int number = int.Parse(n);
+				numbers.Add(number);
+			}
+		}
+
+		public int SolveTwoNumbers()
+		{
+			List<int> numbers = new List<int>();
+
+			ReadInputs(inputPath, ref numbers);
+
+			for (int i = 0; i < numbers.Count; ++i)
+			{
+				int remaining = 2020 - numbers[i];
+				for (int j = i + 1; j < numbers.Count; ++j)
+				{
+					if (remaining == numbers[j])
+					{
+						Console.WriteLine($"Found numbers: {numbers[i]} and {numbers[j]}; multiplication = {numbers[i] * numbers[j]}");
+						return numbers[i] * numbers[j];
+					}
+				}
+			}
+
+			return -1;
+		}
+
+		public int SolveThreeNumbers()
+		{
+			List<int> numbers = new List<int>();
+
+			ReadInputs(inputPath, ref numbers);
+
+			for (int i = 0; i < numbers.Count; ++i)
+			{
+				int remaining = 2020 - numbers[i];
+				for (int j = i + 1; j < numbers.Count; ++j)
+				{
+					int rem = remaining - numbers[j];
+					for (int p = j + 1; p < numbers.Count; ++p)
+					{
+						if (rem == numbers[p])
+						{
+							Console.WriteLine($"Found numbers: {numbers[i]} and {numbers[j]} and {numbers[p]}; multiplication = {numbers[i] * numbers[j] * numbers[p]}");
+							return numbers[i] * numbers[j] * numbers[p];
+						}
+					}
+				}
+			}
+
+			return -1;
+		}
+	}
+}
--- Advent1/Advent1/Program.cs
+++ Advent1/Advent1/Program.cs
@@ -6,47 +6,12 @@ namespace Advent1
 {
 	class Program
 	{
-		static void ReadInputs(string file, ref List<int> numbers)
-		{
-			if(numbers == null)
-			{
-				numbers = new List<int>();
-			}
-
-			numbers.Clear();
-
-			string[] lines = File.ReadAllLines(file);
-
-			foreach(string n in lines)
-			{
-				int number = int.Parse(n);
-				numbers.Add(number);
-			}
-		}
-
 		static void Main(string[] args)
 		{
-			List<int> numbers = new List<int>();
+			Day1 day1 = new Day1();
 
-			ReadInputs("../../../../Input/Input1a.txt", ref numbers);
-
-			for (int i = 0; i < numbers.Count; ++i)
-			{
-				int remaining = 2020 - numbers[i];
-				for(int j = i + 1; j < numbers.Count; ++j)
-				{
-					int rem = remaining - numbers[j];
-					for(int p = j + 1; p < numbers.Count; ++p)
-					{
-						if (rem == numbers[p])
-						{
-							Console.WriteLine($"Found numbers: {numbers[i]} and {numbers[j]} and {numbers[p]}; multiplication = {numbers[i] * numbers[j] * numbers[p]}");
-							Console.ReadKey();
-							return;
-						}
-					}
-				}
-			}
+			Console.WriteLine($"TwoNumbers: {day1.SolveTwoNumbers()}");
+			Console.WriteLine($"ThreeNumbers: {day1.SolveThreeNumbers()}");
 
 			// Wait for keypress
 			Console.ReadKey();
